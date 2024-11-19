6e6ade587424ac2abb9ffdcc56c023b5f394e557
MYXAMOR338 <silova155@gmail.com>
2024-11-19 23:24:41 +0300

    no std +

--- SocketArgs.h
+++ SocketArgs.h
@@ -8,10 +8,8 @@ class SocketArgs {
 public:
     SocketArgs(size_t bufferSize);
     void SetBuffer(const std::vector<uint8_t>& buffer);
-
-    std::vector<uint8_t> Buffer;
     bool IsInUse = false;
-    std::string RecipientID;
+
 
 private:
     size_t bufferSize;
